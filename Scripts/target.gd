e342c1939c77a6e53df0b0ddd102c2ef15ad768a
Alberto Lucchini <albilucchi@icloud.com>
2025-08-22 18:56:20 +0200

    Game Loop Update

--- Scripts/target.gd
+++ Scripts/target.gd
@@ -2,10 +2,13 @@ extends Node2D
 
 var target_entered : bool = false
 
+
+
 func _input(event):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
 		if target_entered == true:
 			queue_free()
+			Global.player_score += 1
 
 
 func _on_target_hitbox_mouse_entered() -> void:

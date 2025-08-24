816f6a229e12f1d862accc91c5926440e72a9d04
Fire.M64 <matteomanulitindaro@outlook.it>
2025-08-24 17:17:17 +0200

    Updated Input event

--- Scripts/fucker.gd
+++ Scripts/fucker.gd
@@ -6,10 +6,11 @@ var entered_graze : bool = false #CHECKS IF GRAZE HITBOX IS ENTERED
 
 func _input(event):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
-		if entered_hit:
-			print("HIT FUCKER")
-		if entered_graze and !entered_hit:
-			print("GRAZED FUCKER")
+		if event.pressed:
+			if entered_hit:
+				print("HIT FUCKER")
+			if entered_graze and !entered_hit:
+				print("GRAZED FUCKER")
 
 
 #GRAZE ENTERED / EXITED
--- Scripts/target.gd
+++ Scripts/target.gd
@@ -1,16 +1,20 @@
 extends Node2D
 
 var target_entered : bool = false
-
+@export var health : int = 1
 
 
 func _input(event):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
 		if target_entered == true:
-			print("TARGET HIT")
-			queue_free()
-			Global.player_score += 1
+			if event.pressed:
+				print("TARGET HIT")
+				health -= 1
+				Global.player_score += 1
 
+func _process(_delta):
+	if health <= 0:
+		queue_free()
 
 func _on_target_hitbox_mouse_entered() -> void:
 	target_entered = true

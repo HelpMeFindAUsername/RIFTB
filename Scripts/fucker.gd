aa09f698045e2d95f79a1adf85883a39a41a57b1
Fire.M64 <matteomanulitindaro@outlook.it>
2025-08-24 12:55:19 +0200

    Basic Fucker hit system

---
+++ Scripts/fucker.gd
@@ -0,0 +1,30 @@
+extends Node2D
+
+var entered_hit : bool = false #CHECKS IF HITBOX IS ENTERED
+var entered_graze : bool = false #CHECKS IF GRAZE HITBOX IS ENTERED
+
+
+func _input(event):
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		if entered_hit:
+			print("HIT FUCKER")
+		if entered_graze and !entered_hit:
+			print("GRAZED FUCKER")
+
+
+#GRAZE ENTERED / EXITED
+func _on_fucker_graze_hitbox_mouse_entered():
+	entered_graze = true
+
+
+func _on_fucker_graze_hitbox_mouse_exited():
+	entered_graze = false
+
+
+#HITBOX ENTERED / EXITED
+func _on_fucker_hit_box_mouse_entered():
+	entered_hit = true
+
+
+func _on_fucker_hit_box_mouse_exited():
+	entered_hit = false
--- Scripts/target.gd
+++ Scripts/target.gd
@@ -7,13 +7,13 @@ var target_entered : bool = false
 func _input(event):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
 		if target_entered == true:
+			print("TARGET HIT")
 			queue_free()
 			Global.player_score += 1
 
 
 func _on_target_hitbox_mouse_entered() -> void:
 	target_entered = true
-
 
 
 func _on_target_hitbox_mouse_exited() -> void:

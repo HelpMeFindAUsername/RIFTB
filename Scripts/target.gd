ae2077cccf255aec9904a0d098dcaa7f52be8803
Fire.M64 <matteomanulitindaro@outlook.it>
2025-08-25 14:50:14 +0200

    Started Animation Work

--- Scripts/target.gd
+++ Scripts/target.gd
@@ -18,9 +18,7 @@ func _process(_delta):
 
 func _on_target_hitbox_mouse_entered() -> void:
 	target_entered = true
-	print("Entered_Target")
 
 
 func _on_target_hitbox_mouse_exited() -> void:
 	target_entered = false
-	print("Exited_Target")

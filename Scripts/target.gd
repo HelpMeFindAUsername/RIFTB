6101fe4cece627ce2a1a65eda424e229bc83d26a
Alberto Lucchini <albilucchi@icloud.com>
2025-08-26 02:06:01 +0200

    Fucking Fragmentation Bluushit

--- Scripts/target.gd
+++ Scripts/target.gd
@@ -6,8 +6,13 @@ var target_entered : bool = false
 @onready var animation_player = $AnimationPlayer
 @onready var target = $".."
 @export var scale_mult := Vector2(1, 1)
+@onready var random_sprite: Sprite2D = $Random_Sprite
+@onready var target_hitbox: Area2D = $Random_Sprite/Target_Hitbox
 
 var hit_force := Vector2(1, 1)
+
+# preload fragment scene
+@export var fragment_scene = load("res://Scenes/2D/Sub/Target_Fragment/target_fragment.tscn")
 
 func _input(event):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
@@ -21,6 +26,14 @@ func _input(event):
 				print("TARGET DESTROYED")
 				health -= 1
 				Global.player_score += points_assigned
+			
+			if event.pressed and health <= -3:
+				print("TARGET FUCKED UP")
+				target_entered = false
+				target_hitbox.queue_free()
+				random_sprite.queue_free()
+				_spawn_fragments() # call fragments spawn
+
 
 func _process(_delta):
 	if health <= 0:
@@ -35,6 +48,23 @@ func _physics_process(_delta):
 func _on_target_hitbox_mouse_entered() -> void:
 	target_entered = true
 
-
 func _on_target_hitbox_mouse_exited() -> void:
 	target_entered = false
+
+# -----------------
+# Fragment Spawner
+# -----------------
+func _spawn_fragments():
+	if fragment_scene == null:
+		return
+	
+	for i in range(4): # random number of fragments
+		var frag = fragment_scene.instantiate()
+		get_parent().add_child(frag)
+		frag.global_position = global_position
+		
+		# give random velocity & rotation
+		if frag is RigidBody2D:
+			var impulse = Vector2(randf_range(-200, 200), randf_range(-200, -50))
+			frag.apply_impulse(impulse)
+			frag.angular_velocity = randf_range(-5.0, 5.0)

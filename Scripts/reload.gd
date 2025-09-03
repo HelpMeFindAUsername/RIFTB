cb2931f94f6250e43b2a11dc969d1e856298ee1f
Fire.M64 <matteomanulitindaro@outlook.it>
2025-09-03 17:23:02 +0200

    Added Crossair

--- Scripts/reload.gd
+++ Scripts/reload.gd
@@ -1,6 +1,6 @@
 extends Timer
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("reload"):
 		if Global.bullets:
 			Global.bullets = 1
